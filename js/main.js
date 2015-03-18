76aa1d820a31e069739fc5dcd5fd8265f51d5ec1
Fahim Chowdhury <fahim.chowdhury1985@gmail.com>
2015-03-18 22:59:28 +0000

    added groups and styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function(){
-	var Main=
+	var Main=window.Main=
 	{
 		username:'',
 		userCollections:[],
@@ -13,14 +13,15 @@
 			document.addEventListener('DOMContentLoaded',this.onLoad.bind(this));
 		},
 		onLoad:function(){
+			window.skipRTCMultiConnectionLogs = true;
 			this.setListeners();
 			var muteButton = document.getElementById('muteButton');
 			var speakerButton = document.getElementById('speakerButton');
 			this.audioHolder = document.getElementById('audioHolder');
 			this.usersHolder = document.getElementById('users');
 			this.groupsHolder= document.getElementById('rooms-list');
-			var connection = this.connection = new RTCMultiConnection();
-
+			var connection = this.connection = new RTCMultiConnection('OfficeTalk');
+			connection.dontOverrideSession = true;
 
 			connection.session = {
 				audio: true
@@ -30,53 +31,41 @@
 				OfferToReceiveAudio: true,
 				OfferToReceiveVideo: false
 			};
+			connection.transmitRoomOnce = false;
+			connection.isAcceptNewSession = true;
+			 //connection.keepStreamsOpened = true;
+			 connection.onstream =this.onUserConnected.bind(this);
+			 connection.onstreamended =this.onUserDisconnected.bind(this);
+			 connection.onNewSession = this.onGroupList.bind(this);
+			 connection.onSessionClosed = this.onGroupClosed.bind(this);
+			 connection.log = false;
+			 console.log("loaded",connection);
 
-			connection.onstream =this.onUserConnected.bind(this);
-			connection.onstreamended =this.onUserDisconnected.bind(this);
-			connection.onNewSession = this.onGroupList.bind(this);
-			connection.onSessionClosed = this.onGroupClosed.bind(this);
-			console.log("loaded",connection);
 
-			
 
-			
-			var muted=false;
-			var remoteMute=false;
-			function onMuteClicked(){
-				if(!muted)
-				{
-					connection.streams.mute({
-						audio: true,
-						type: 'local'
-					});
-					muted=true;
-				}else{
-					connection.streams.unmute({
-						audio: true,
-						type: 'local'
-					});
-					muted=false;
-				}
-			}
-			function onRemoteMuteClicked(){
-				if(!remoteMute)
-				{
-					connection.streams.mute({
-						audio: true,
-						type: 'remote'
-					});
-					remoteMute=true;
-				}else{
-					connection.streams.unmute({
-						audio: true,
-						type: 'remote'
-					});
-					remoteMute=false;
-				}
-			}
 
-			muteButton.addEventListener('click',onMuteClicked);
-			speakerButton.addEventListener('click',onRemoteMuteClicked);
+			 var muted=false;
+			 var remoteMute=false;
+
+			 function onRemoteMuteClicked(){
+			 	if(!remoteMute)
+			 	{
+			 		connection.streams.mute({
+			 			audio: true,
+			 			type: 'remote'
+			 		});
+			 		remoteMute=true;
+			 	}else{
+			 		connection.streams.unmute({
+			 			audio: true,
+			 			type: 'remote'
+			 		});
+			 		remoteMute=false;
+			 	}
+			 }
+
+			 //muteButton.addEventListener('click',onMuteClicked);
+			 //speakerButton.addEventListener('click',onRemoteMuteClicked);
         // setup signaling to search existing sessions
         connection.connect();
 
@@ -97,23 +86,24 @@
     },
     onJoinGroup:function(event){
     	var sessionid = event.target.getAttribute('data-sessionid');
-    		session = this.sessions[sessionid];
+    	session = this.sessions[sessionid];
 
-    		if (!session) throw 'No such session exists.';
-    		this.connection.extra={
-    			'username':this.username
-    		}
-    		this.connection.join(session);
+    	if (!session) throw 'No such session exists.';
+    	this.connection.extra={
+    		'username':this.username
+    	}
+    	this.connection.join(session);
     	document.querySelector(".view.groups").classList.remove("show");
     	document.querySelector(".view.session").classList.add("show");
     },
     onGroupList:function(session){
+    	console.log('onGroupList',session);
     	if (this.sessions[session.sessionid]) return;
     	this.sessions[session.sessionid] = session;
 
     	var tr = document.createElement('tr');
     	tr.innerHTML = '<td>' + session.extra['session-name'] + '</td>' +
-    	'<td><button class="join">Join</button></td>';
+    	'<td><button class="join btn btn-primary">Join</button></td>';
     	this.groupsHolder.insertBefore(tr, this.groupsHolder.firstChild);
 
     	var joinRoomButton = tr.querySelector('.join');
@@ -121,7 +111,7 @@
     	joinRoomButton.onclick = this.onJoinGroup.bind(this);
     },
     onGroupClosed:function(event){
-console.log("onGroupClosed",event);
+    	console.log("onGroupClosed",event);
     },
     onCreateGroup:function(){
     	this.connection.extra = {
@@ -133,18 +123,78 @@ console.log("onGroupClosed",event);
     	document.querySelector(".view.session").classList.add("show");
     },
     onUserDisconnected:function(event){
-console.log("onUserDisconnected",event);
+    	console.log("onUserDisconnected",event);
+    	if(this.userCollections[event.userid]){
+    		var li = this.userCollections[event.userid].element;
+    		var mediaElement =document.querySelector('media-userid');
+    		this.usersHolder.removeChild(li);
+    		if(li)this.usersHolder.removeChild(mediaElement);
+    		if(mediaElement)this.userCollections[event.userid]=null;
+    		console.log("removed");
+    	}
+    },
+    createUserLI:function(name,id){
+    	var li = document.createElement('LI');
+    	li.className="participant-row";
+    	li.setAttribute('userid',id);
+    	li.innerHTML='<span>'+name+'</span>'+
+    	'<i class="icon-mic" onclick="Main.onMuteClicked(this)" user-id="'+id+'"></i>'
+    	this.usersHolder.appendChild(li);
+
+    	return li;
+    },
+    onMuteClicked:function(element){
+    	var id =element.getAttribute('user-id');
+    	var user =this.userCollections[id];
+    	console.log(user);
+    	if(!user)return;
+    	if(!user.muted)
+    	{
+
+    		user.data.rtcMultiConnection.streams.mute({
+    			audio: true,
+    			type: 'local'
+    		});
+    		user.muted=true;
+    		element.classList.remove("icon-mic");
+    		element.classList.add("icon-mic-off");
+    	}else{
+    		user.data.rtcMultiConnection.streams.unmute({
+    			audio: true,
+    			type: 'local'
+    		});
+    		user.muted=false;
+    		element.classList.remove("icon-mic-off");
+    		element.classList.add("icon-mic");
+    	}
+
     },
     onUserConnected:function(event){
     	if(event.extra.username){
-    		var li = document.createElement('LI');
-    		li.innerHTML=event.extra.username;
-    		this.usersHolder.appendChild(li);
+    		
+    		var li = this.createUserLI(event.extra.username,event.userid);
+    		this.userCollections[event.userid]={
+    			data:event,
+    			element:li,
+    			mediaElement:event.mediaElement,
+    			muted:false
+    		};
     	}
-    	
-    	console.log(event.extra.username);
-    	this.audioHolder.insertBefore(event.mediaElement, this.audioHolder.firstChild);
-    }
-};
-Main.init();
-})();
+    	if(event.type == 'remote')
+    	{
+    		this.connection.isAcceptNewSession = true;
+    		this.connection.askToShareParticipants();
+    	}
+    	if (this.connection.isInitiator && event.type == 'remote' ) {
+                    // call "shareParticipants" to manually share participants with all connected users!
+                    this.connection.shareParticipants({
+                    	dontShareWith: event.userid
+                    });
+                }
+                console.log('onUserConnected',event);
+                event.mediaElement.setAttribute('media-userid',event.userid);
+                this.audioHolder.insertBefore(event.mediaElement, this.audioHolder.firstChild);
+            }
+        };
+        Main.init();
+    })();
